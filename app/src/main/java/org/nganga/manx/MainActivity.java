7eeb5f639009dee377fc83e54a563d9974a8b31e
nganga <mtnjoroge@gmail.com>
2015-09-25 18:14:00 +0300

    Logged first song details

--- app/src/main/java/org/nganga/manx/MainActivity.java
+++ app/src/main/java/org/nganga/manx/MainActivity.java
@@ -1,16 +1,43 @@
 package org.nganga.manx;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint(Config.API_URL).build();
+        SCService scService = restAdapter.create(SCService.class);
+        scService.getRecentTracks(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date()), new Callback<List<Track>>() {
+            @Override
+            public void success(List<Track> tracks, Response response) {
+                Log.d(TAG, "First track title: " + tracks.get(0).getTitle());
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.d(TAG, "Error: " + error);
+            }
+        });
     }
 
     @Override
